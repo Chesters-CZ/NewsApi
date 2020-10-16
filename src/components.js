ba9c58e81e8dd4d329d47d05f07a2d647ec36b18
Chesters-CZ <bezouskavit@gmail.com>
2020-10-16 11:52:38 +0200

    trochu debilní, ale už napůl hezký. každopádně parsování funguje.

--- src/components.js
+++ src/components.js
@@ -1,13 +1,16 @@
 
 
-export function generateNewsBox(title, description) {
+export function generateNewsBox(title, description, author, publishedAt, sourcename, link) {
     let container = document.createElement('div');
-    let header = document.createElement('h2');
+    let header = document.createElement('a');
     header.innerText = title;
     let desc = document.createElement('p');
     desc.innerText = description;
+    let src = document.createElement('h5');
+    src.innerText = "Published at " + publishedAt + " by " + author + " for " + sourcename;
 
     container.appendChild(header);
+    container.appendChild(src);
     container.appendChild(desc);
 
     return container;
--- src/index.js
+++ src/index.js
@@ -1,53 +1,24 @@
 import {generateNewsBox} from "./components";
 
-const articles = [
-    {
-        title: 'Article 1',
-        description: 'Lorem ipsum1'
-    },
-    {
-        title: 'Article 2',
-        description: 'Lorem ipsum2'
-    },
-    {
-        title: 'Article 3',
-        description: 'Lorem ipsum3'
-    },
-    {
-        title: 'Article 4',
-        description: 'Lorem ipsum4'
-    },
-    {
-        title: 'Article 5',
-        description: 'Lorem ipsum5'
-    },
-    {
-        title: 'Article 6',
-        description: 'Lorem ipsum6'
-    },
-    {
-        title: 'Article 7',
-        description: 'Lorem ipsum7'
-    },
-    {
-        title: 'Article 8',
-        description: 'Lorem ipsum8'
-    },
-];
+const axios = require("axios");
 
 window.onload = function() {
-    const header = document.createElement('h1'); // <h1>Welcome</h1>
-    header.innerText = "Welcome";
-    document.body.appendChild(header);
+    const odaxiosu = axios.get("http://newsapi.org/v2/top-headlines?country=cz&apiKey=a35c735a4617414e8031a21950572432").then(function(res) {
+        const articls =  res.data.articles;
+        /*const header = document.createElement('h1'); // <h1>Welcome</h1>
+        header.innerText = "Welcome";
+        document.body.appendChild(header);
 
-    const news = generateNewsBox("My first news", "Hello blablabla");
-    document.body.appendChild(news);
+        const news = generateNewsBox("My first news", "Hello blablabla");
+        document.body.appendChild(news);*/
 
-    let ble;
-    let fuj;
-    for(let i=0; i<articles.length; i++) {
-        ble = generateNewsBox(articles[i].title, articles[i].description);
-        fuj = document.getElementById("div");
-        fuj.appendChild(ble);
-    }
+        let ble;
+        let fuj;
+        for(let i=0; i<articls.length; i++) {
+            ble = generateNewsBox(articls[i].title, articls[i].description, articls[i].author, articls[i].publishedAt, articls[i].source.name, articls[i].url);
+            fuj = document.getElementById("div");
+            fuj.appendChild(ble);
+        }
+    });
+
 }
